6fab54e4611bba7b0b3b1dd64f7b1def298f7fea
douglas oliveira <douglasgama0@gmail.com>
2022-01-28 14:03:09 -0300

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="content-footer">
       <div className="icon-footer" name="contato">
-        <a className="link-footer" href="https://github.com/Doug77">
+        <a className="link-footer" target="_blank" href="https://github.com/Doug77" rel="noreferrer">
           <img
             src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="github-icon"
             width="30px"
@@ -14,7 +14,7 @@ export default function Footer() {
             GitHub
           </span>
         </a>
-        <a className="link-footer" href="https://www.linkedin.com/in/douglas-d-oliveira/">
+        <a className="link-footer" target="_blank" href="https://www.linkedin.com/in/douglas-d-oliveira/" rel="noreferrer">
           <img 
             src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="linkedin-icon"
             width="30px"
