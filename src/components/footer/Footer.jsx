6688c50e1a54123fb84f2862fa177e6c410c0d05
douglas oliveira <douglasgama0@gmail.com>
2022-01-28 13:52:02 -0300

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Footer.css';
 
 export default function Footer() {
   return (
     <footer className="content-footer">
       <div className="icon-footer" name="contato">
-        <Link className="link-footer" to={{ pathname: "https://github.com/Doug77" }} target="_blank" >
+        <a className="link-footer" href="https://github.com/Doug77">
           <img
             src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="github-icon"
             width="30px"
@@ -14,8 +13,8 @@ export default function Footer() {
           <span className="text-footer">
             GitHub
           </span>
-        </Link>
-        <Link className="link-footer" to={{ pathname: "https://www.linkedin.com/in/douglas-d-oliveira/" }} target="_blank" >
+        </a>
+        <a className="link-footer" href="https://www.linkedin.com/in/douglas-d-oliveira/">
           <img 
             src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="linkedin-icon"
             width="30px"
@@ -23,7 +22,7 @@ export default function Footer() {
           <span className="text-footer">
             LinkedIn
           </span>
-        </Link>
+        </a>
       </div>
       <div className="name-footer">
         Desenvolvido por Douglas Oliveira
